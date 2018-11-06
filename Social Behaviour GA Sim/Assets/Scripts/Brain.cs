34b4841fba7e4f3904824a381341b38783c7ea4e
Benjy96 <benjamindickey@hotmail.co.uk>
2018-11-06 18:28:23 +0000

    Fix gene numbering
    For each decision, there must be a gene to run an instruction.
    
    I was confining movement related instructions to see walkable items, but how does the agent move if they see an enemy?
    
    I added all the "state bools" (seeOther, seeWalkable) to movement handling - so with 3 binary environmental switches, there are 6 movement decisions to make

--- Social Behaviour GA Sim/Assets/Scripts/Brain.cs
+++ Social Behaviour GA Sim/Assets/Scripts/Brain.cs
@@ -15,7 +15,7 @@ public class Brain : MonoBehaviour
     public DNA dna;
     private Body body;
     
-    private int DNALength = 6;   //dna length 6 because we have 6 decisions to make
+    private int DNALength = 7;   //dna length 7 because 7 decisions currently implemented
     private int dnaValues = 3;
    
     bool alive = true;
@@ -76,19 +76,22 @@ public class Brain : MonoBehaviour
             if (!hit.collider.gameObject.tag.Equals("Walkable"))
             {
                 seeObstacle = true;
-                RunMovementGenes();
+                
             }
             else if (hit.collider.gameObject.tag.Equals("Bot"))
             {
                 seeOther = true;
                 other = hit.collider.gameObject;
-                RunInteractionGenes(other);
             }
         }
+
+        RunInteractionGenes(other);
+        RunMovementGenes();
     }
 
     /// <summary>
     /// Control the movement related genes (Each gene represents a possible instruction to run)
+    /// 3 bools (true or false) means 6 genes - 6 decisions to be made
     /// </summary>
     private void RunMovementGenes()
     {
@@ -108,6 +111,32 @@ public class Brain : MonoBehaviour
             else if (dna.GetGene(1) == (int)GeneInstructions.MovementRight) turn = 90;
         }
 
+        if (seeOther)
+        {
+            if (dna.GetGene(2) == (int)GeneInstructions.MovementForward) move = 1;
+            else if (dna.GetGene(2) == (int)GeneInstructions.MovementLeft) turn = -90;
+            else if (dna.GetGene(2) == (int)GeneInstructions.MovementRight) turn = 90;
+        }
+        else
+        {
+            if (dna.GetGene(3) == (int)GeneInstructions.MovementForward) move = 1;
+            else if (dna.GetGene(3) == (int)GeneInstructions.MovementLeft) turn = -90;
+            else if (dna.GetGene(3) == (int)GeneInstructions.MovementRight) turn = 90;
+        }
+
+        if (seeResource)
+        {
+            if (dna.GetGene(4) == (int)GeneInstructions.MovementForward) move = 1;
+            else if (dna.GetGene(4) == (int)GeneInstructions.MovementLeft) turn = -90;
+            else if (dna.GetGene(4) == (int)GeneInstructions.MovementRight) turn = 90;
+        }
+        else
+        {
+            if (dna.GetGene(5) == (int)GeneInstructions.MovementForward) move = 1;
+            else if (dna.GetGene(5) == (int)GeneInstructions.MovementLeft) turn = -90;
+            else if (dna.GetGene(5) == (int)GeneInstructions.MovementRight) turn = 90;
+        }
+
         transform.Translate(0, 0, move * 0.1f);
         transform.Rotate(0, turn, 0);
     }
@@ -119,18 +148,19 @@ public class Brain : MonoBehaviour
             //TODO: add inner options, such as see colour of enemy and take action depending on their colour
             Body otherBody = other.GetComponent<Body>();
 
-            if (dna.GetGene(2) == (int)GeneInstructions.InteractionAttack)
+            if (dna.GetGene(6) == (int)GeneInstructions.InteractionAttack)
             {
                 otherBody.Damage(20);   //todo: remove hard coding
+                Debug.DrawLine(transform.position, other.transform.position, Color.green);
             }
-            else if (dna.GetGene(2) == (int)GeneInstructions.InteractionGive)
+            else if (dna.GetGene(6) == (int)GeneInstructions.InteractionGive)
             {
                 otherBody.Feed(20);
                 //TODO: Add benefits of feeding, e.g.:
                     //1. Increased "visual" indicator of friendliness
                     //2. Money from other
             }
-            else if (dna.GetGene(2) == (int)GeneInstructions.InteractionIgnore) return;
+            else if (dna.GetGene(6) == (int)GeneInstructions.InteractionIgnore) return;
         }
     }
 
